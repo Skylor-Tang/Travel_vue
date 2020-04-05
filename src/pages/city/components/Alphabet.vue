b85de39aeae8b7b1fd3c00320de201385dee5e43
skylor <739230854@qq.com>
2020-04-05 16:34:50 +0800

    ajax city

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,19 +1,26 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li
+      class="item"
+      v-for="(item, key) of cities"
+      :key="key"
+    >
+    {{key}}
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/variables.styl'
   .list
     display: flex
     flex-direction: column
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -7,72 +7,36 @@
           <div class="button-wrapper">
             <div class="button">北京</div>
           </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
         </div>
       </div>
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
+          <div
+            class="button-wrapper"
+            v-for="item of hot"
+            :key="item.id"
+          >
+              <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="button-list">
+      <div
+        class="area"
+        v-for="(item, key) of cities"
+        :key="key"
+      >
+        <div class="title border-topbottom">{{key}}</div>
           <div class="item-list">
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
+            <div
+              class="item border-bottom"
+              v-for='innerItem  of item'
+              :key="innerItem.id"
+            >
+              {{innerItem.name}}
+            </div>
           </div>
         </div>
-        <div class="title border-topbottom">A</div>
-        <div class="button-list">
-          <div class="item-list">
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-          </div>
-        </div>
-        <div class="title border-topbottom">A</div>
-        <div class="button-list">
-          <div class="item-list">
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -81,6 +45,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityHeader',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
@@ -98,12 +66,12 @@ export default {
     &:before
       border-color: #ccc
   .list
-      overflow: hidden
-      position: absolute
-      top: 1.58rem
-      left: 0
-      right: 0
-      bottom: 0
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
     .title
       line-height: .54rem
       background: #eee
@@ -122,10 +90,6 @@ export default {
           text-align: center
           border: .02rem solid #ccc
           border-radius: .06rem
-    .item-list
-      .item
-        line-height: .76rem
-        padding-left: .2rem
     .item-list
       .item
         line-height: .76rem
