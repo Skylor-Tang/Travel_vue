eacba906bdd9f2895f16faf807cc50e07116b239
skylor <739230854@qq.com>
2020-04-08 14:26:20 +0800

    use vuex

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,7 +9,7 @@
     </div>
     <router-link to="/city">
       <div class="header-right">
-        {{city}}
+        {{this.city}}
         <span class="iconfont arrow-icon">&#xe600;</span>
       </div>
     </router-link>
@@ -17,10 +17,11 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  computed: {
+    ...mapState(['city'])
   }
 }
 </script>
@@ -49,7 +50,8 @@ export default {
       border-radius : .1rem
       color : #ccc
     .header-right
-      width : 1.24rem
+      min-width : 1.04rem
+      padding : 0 .1rem
       float :right
       text-align : center
       color : #fff
